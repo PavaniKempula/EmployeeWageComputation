65b635d7c09639611f3276c0a4efcc619f659c7b
PavaniKempula <pavanikempula1998@gmail.com>
2021-05-12 14:17:53 +0530

    Add : Store the Day and the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -26,6 +26,18 @@ NUM_WORKING_DAYS=20;
 totalWorkHours=0;
 totalWorkingDays=0;
 
+
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+declare -A dailywage
+
 function getWorkHrs() {
         case $1 in
                 $IS_FULL_TIME)
@@ -49,10 +61,9 @@ do
         empCheck=$((RANDOM%3))
         getWorkHrs $empCheck
         totalEmpHrs=$(($totalEmpHrs + $empHrs))
-        dailyWages[$totalWorkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
+        dailyWage["$totalWorkingDays"]=$(($empHrs*$EMP_RATE_PER_HR))
 done
 
 totalSalary="$( getEmpWage $totalEmpHrs )"
-
 
 
